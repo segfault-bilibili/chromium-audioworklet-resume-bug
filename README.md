c168dcdfa139459886ad2ad8b56014282bad2516
segfault-bilibili <80713224+segfault-bilibili@users.noreply.github.com>
2022-04-01 22:51:12 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# chromium-audioworklet-resume-bug
-Chromium AudioWorklet resume bug
+# Chromium AudioWorklet resume bug
+
+In `resume-bug.html`, click "Init AudioContext" button and "Play" button in turn to play tide-like white noise.
+
+*"Tide-like" means: **it starts in silence**, and then sounds gradually more and more louder, then turns to be less and less louder, and so on.*
+
+The expected behavior is just as described above: **it should always start in silence** - however, I can hear a very short "residue" burst in the beginning if it's stopped first and then restarted.
+
+I encountered this problem in Chrome (100.0.4896.60); Firefox (98.0.2) didn't appear to have this problem.
---
+++ resume-bug-processor.js
@@ -0,0 +1,36 @@
+class ResumeBugProcessor extends AudioWorkletProcessor {
+    isPlaying = false;
+    counter = 0;
+  
+    constructor (...args) {
+        super(...args);
+        this.port.onmessage = (ev) => {
+            this.isPlaying = ev.data ? true : false;
+            this.counter = 0;
+            this.port.postMessage("done");
+        }
+    }
+  
+    process (inputs, outputs, parameters) {
+        if (!this.isPlaying) return true;
+
+        const period = 2 * 48000 / 128;
+        const halfPeriod = period / 2;
+
+        if (this.counter < period) this.counter++;
+        else this.counter = 0;
+        const counter = this.counter;
+
+        const volume = (counter < halfPeriod ? counter : (period - counter)) / halfPeriod;
+
+        const random = new Float32Array(outputs[0][0].length);
+        for (let i = 0; i < random.length; i++) random[i] = (Math.random() * 2 - 1) * volume;
+
+        outputs[0].forEach((channel) => channel.set(random));
+
+        return true;
+    }
+  }
+  
+  
+  registerProcessor('resume-bug-processor', ResumeBugProcessor);
